ad29cc0b1f06509c8b3b2c6d014b4477ff198b98
natya2203 <nastya_2203@ukr.net>
2019-12-07 22:51:38 +0200

    Updates

---
+++ precache-manifest.9dcb6eba99b6e5dd025087765cf63542.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534cd35a013d585c273bc78957f3b3d5",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "61a1c7de40daeabb315c",
+    "url": "/react_tabs/static/css/main.5e50ef26.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "61a1c7de40daeabb315c",
+    "url": "/react_tabs/static/js/main.1db62c42.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
